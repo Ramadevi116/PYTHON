647a0949ba7eca0c498a21716c2854f3840493e1
hi <pokalaramadevi116@gmail.com>
2021-08-25 18:39:23 +0530

    Function.pro

--- Python/Functions/function.py
+++ Python/Functions/function.py
@@ -1,1 +1,55 @@
 # functions...
+# Dance performance....judges grades...And the final winner...
+# with out parameters and with out return type...
+def dance():           
+    Contestants=["Ramadevi","Akshaya","Bhavitha"]
+    judges=["Monal","Baba bhaskar","Anee master","Mumaith khan","Yash master"]
+    len1=len(Contestants)
+    len2=len(judges)
+    Ramadevi_score=[]
+    Akshaya_score=[]
+    Bhavitha_score=[]
+    print("Judges score:")
+    for i in range(1):
+         for j in range(len2):
+            score=int(input("Enter score of 10:"))
+            print(Contestants[i],"got score",score,"from",judges[j])
+            Ramadevi_score.append(score)
+            print(Ramadevi_score)
+    for i in range(1,2):
+         for j in range(len2):
+            score=int(input("Enter score of 10:"))
+            print(Contestants[i],"got score",score,"from",judges[j])
+            Akshaya_score.append(score)
+            print(Akshaya_score)
+    for i in range(2,3):
+         for j in range(len2):
+            score=int(input("Enter score of 10:"))
+            print(Contestants[i],"got score",score,"from",judges[j])
+            Bhavitha_score.append(score)
+            print(Bhavitha_score)
+    score1=0
+    for i in Ramadevi_score:
+        score1+=i
+    print(score1)
+    score2=0
+    for j in Akshaya_score:
+        score2+=j
+    print(score2)
+    score3=0
+    for k in Bhavitha_score:
+        score3+=k
+    print(score3)
+
+    if(score1>score2):
+        if(score1>score3):
+            print("Today dance + winner is Ramadevi...🎉🎉")
+        else:
+            pass
+    elif(score2>score1):
+        if(score2>score3):
+            print("Today dance + winner is Akshaya...🎉🎉")
+    else:
+        print("Today dance + winner is Bhavitha...🎉🎉")
+
+dance()
